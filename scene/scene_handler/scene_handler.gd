561cc66ba12d0e867af35682dbb6cbfa4cfd8c53
Stefan Karczewski <s27459@pjwstk.edu.pl>
2025-06-09 21:08:24 +0200

    Clean up scene_handler.gd

--- scene/scene_handler/scene_handler.gd
+++ scene/scene_handler/scene_handler.gd
@@ -1,22 +1,21 @@
 extends Node
 
 func _ready():
-	
 	$MainMenu.visible = true
 	$Settings.visible = false
 	$HowToPlay.visible = false
-	print($Settings/MarginContainer/VBoxContainer/BackButton)
-	print($HowToPlay/MarginContainer/VBoxContainer/BackButton)
+	
 	$MainMenu/MarginContainer/VBoxContainer/PlayButton.pressed.connect(on_new_game_pressed)
 	$MainMenu/MarginContainer/VBoxContainer/ExitButton.pressed.connect(on_exit_pressed)
 	$MainMenu/MarginContainer/VBoxContainer/SettingsButton.pressed.connect(func(): navigate("MainMenu", "Settings"))
 	$MainMenu/MarginContainer/VBoxContainer/HowToPlayButton.pressed.connect(func(): navigate("MainMenu", "HowToPlay"))
+	
 	$Settings/MarginContainer/VBoxContainer/BackButton.pressed.connect(func(): navigate("Settings", "MainMenu"))
 	$HowToPlay/MarginContainer/VBoxContainer/BackButton.pressed.connect(func(): navigate("HowToPlay", "MainMenu"))
 
 func on_new_game_pressed():
 	get_node("MainMenu").queue_free()
-	var game_scene = load("res://scene/default.tscn").instantiate()
+	var game_scene = load("res://scene/GameScene.tscn").instantiate()
 	add_child(game_scene)
 	
 func navigate(node, node_to_go):
