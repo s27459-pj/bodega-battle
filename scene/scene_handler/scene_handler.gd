e3de8281dbe8f4ca78d190af352cba5265c3df2e
s27081-pj <115358253+s27081@users.noreply.github.com>
2025-06-09 22:32:39 +0200

    Add music and SFX (#16)

--- scene/scene_handler/scene_handler.gd
+++ scene/scene_handler/scene_handler.gd
@@ -4,23 +4,34 @@ func _ready():
 	$MainMenu.visible = true
 	$Settings.visible = false
 	$HowToPlay.visible = false
-	
+
 	$MainMenu/MarginContainer/VBoxContainer/PlayButton.pressed.connect(on_new_game_pressed)
 	$MainMenu/MarginContainer/VBoxContainer/ExitButton.pressed.connect(on_exit_pressed)
 	$MainMenu/MarginContainer/VBoxContainer/SettingsButton.pressed.connect(func(): navigate("MainMenu", "Settings"))
 	$MainMenu/MarginContainer/VBoxContainer/HowToPlayButton.pressed.connect(func(): navigate("MainMenu", "HowToPlay"))
-	
 	$Settings/MarginContainer/VBoxContainer/BackButton.pressed.connect(func(): navigate("Settings", "MainMenu"))
+	$Settings/MarginContainer/VBoxContainer/VBoxContainer/Apply.pressed.connect(func():  button_click_sfx_effect())
 	$HowToPlay/MarginContainer/VBoxContainer/BackButton.pressed.connect(func(): navigate("HowToPlay", "MainMenu"))
 
+func button_click_sfx_effect():
+	var click = preload("res://assets/audio/sound_effects/coin-collect-retro-8-bit-sound-effect-145251.mp3")
+	$SFXPlayer.stream = click
+	$SFXPlayer.play()
+
 func on_new_game_pressed():
+	button_click_sfx_effect()
 	get_node("MainMenu").queue_free()
+	var new_song = preload("res://assets/audio/music/the-wandering-samurai-344699.mp3")
+	$MusicPlayer.stream = new_song
+	$MusicPlayer.play()
 	var game_scene = load("res://scene/GameScene.tscn").instantiate()
 	add_child(game_scene)
 	
 func navigate(node, node_to_go):
+	button_click_sfx_effect()
 	get_node(node).visible = false
 	get_node(node_to_go).visible = true
 
 func on_exit_pressed():
+	button_click_sfx_effect()
 	get_tree().quit()
