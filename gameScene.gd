7f0071c701123eaf98966ba37afee12b60de4071
KacperPach <39103506+KacperPach@users.noreply.github.com>
2025-06-09 20:32:33 +0200

    added basic enemies (entity, spawning, following path) (#9)

---
+++ gameScene.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+func _ready() -> void:
+	startNextWave()
+
+## Enemy-Wave functions 
+func startNextWave():
+	var waveData = getWaveData()
+	await get_tree().create_timer(0.2).timeout
+	spawnEnemies(waveData)
+	
+func getWaveData():
+	var waveData = [["basic-enemy",2],["basic-enemy",2]] # [nazwa_encji, dalay spawnu następnej encji]
+	return waveData
+
+func spawnEnemies(waveData):
+	for e in waveData:
+		# to jest raczej słabe rozwiązenie że on runtime coś loadujemy ale ¯\_(ツ)_/¯ 
+		var new_enemy = load("res://objects/Enemies/" + e[0] +".tscn").instantiate()
+		#print(new_enemy)
+		$LevelMain.get_node("Enemy-path").add_child(new_enemy)
+		await get_tree().create_timer(e[1]).timeout
