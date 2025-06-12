189ceb3c6992b880297e2936732bbebe45e5cb71
Stefan Karczewski <s27459@pjwstk.edu.pl>
2025-06-12 23:37:19 +0200

    Add a pause menu

---
+++ scene/menu/pause_menu.gd
@@ -0,0 +1,29 @@
+extends Control
+
+func pause():
+	get_tree().paused = true
+	$AnimationPlayer.play("appear")
+
+func resume():
+	get_tree().paused = false
+	$AnimationPlayer.play_backwards("appear")
+
+func togglePause():
+	if get_tree().paused:
+		resume()
+	else:
+		pause()
+
+func _ready() -> void:
+	$AnimationPlayer.play("RESET")
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("escape"):
+		togglePause()
+
+func _on_resume_button_pressed() -> void:
+	resume()
+
+func _on_exit_button_pressed() -> void:
+	resume()
+	get_tree().reload_current_scene()
