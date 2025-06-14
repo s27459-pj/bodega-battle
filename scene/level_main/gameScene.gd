f42dfe45e2d399aa1d82e5f7ed61c2226d34b82e
KacperPach <39103506+KacperPach@users.noreply.github.com>
2025-06-14 10:01:18 +0200

    #17 health UI (#26)
    * added hp-bar scene
    
    * added hp
    
    * fixed-rebase-issue
    
    * added game restart when health == 0
    
    * updated readme

--- scene/level_main/gameScene.gd
+++ scene/level_main/gameScene.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+@onready var ui_health: Node2D = $"UI-Health"
+
 func _ready() -> void:
 	startNextWave()
 
@@ -10,13 +12,14 @@ func startNextWave():
 	spawnEnemies(waveData)
 	
 func getWaveData():
-	var waveData = [["basic-enemy",2],["basic-enemy",2]] # [nazwa_encji, dalay spawnu następnej encji]
+	var waveData = [["basic-enemy",2],["basic-enemy",2],["basic-enemy",2],["basic-enemy",2],["basic-enemy",2],["basic-enemy",2],["basic-enemy",2],["basic-enemy",2],["basic-enemy",2],["basic-enemy",2],["basic-enemy",2],["basic-enemy",2]] # [nazwa_encji, dalay spawnu następnej encji]
 	return waveData
 
 func spawnEnemies(waveData):
 	for e in waveData:
 		# to jest raczej słabe rozwiązenie że on runtime coś loadujemy ale ¯\_(ツ)_/¯ 
 		var new_enemy = load("res://objects/Enemies/" + e[0] +".tscn").instantiate()
+		new_enemy.add_player_health_ref(ui_health)
 		#print(new_enemy)
 		$LevelMain.get_node("Enemy-path").add_child(new_enemy)
 		await get_tree().create_timer(e[1]).timeout
