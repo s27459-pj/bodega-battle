6d142c07ca1811bd6335ceb17510feea62afb35d
Stefan Karczewski <s27459@pjwstk.edu.pl>
2025-06-17 21:44:36 +0200

    Save and load volume to the settings file

--- global/global_audio.gd
+++ global/global_audio.gd
@@ -3,6 +3,26 @@ extends Node
 @onready var music_player: AudioStreamPlayer = $MusicPlayer
 @onready var sfx_player: AudioStreamPlayer = $SFXPlayer
 
+
+# Audio Bus
+func _ready() -> void:
+	var settings = Settings.load()
+	for bus_idx in range(AudioServer.bus_count):
+		var volume = settings.get_value("volume", str(bus_idx))
+		if volume != null:
+			AudioServer.set_bus_volume_db(bus_idx, volume)
+
+func set_bus_volume_db(bus_idx: int, volume_db: float) -> void:
+	AudioServer.set_bus_volume_db(bus_idx, volume_db)
+	var settings = Settings.load()
+	settings.set_value("volume", str(bus_idx), volume_db)
+	Settings.save(settings)
+
+func get_bus_volume_db(bus_idx: int) -> float:
+	return AudioServer.get_bus_volume_db(bus_idx)
+
+
+# Music
 func play_music(stream: AudioStream):
 	music_player.stream = stream
 	music_player.play()
@@ -11,6 +31,8 @@ func stop_music():
 	music_player.stream = null
 	music_player.stop()
 
+
+# SFX
 func play_sfx(stream: AudioStream):
 	sfx_player.stream = stream
 	sfx_player.play()
--- scene/menu/settings.gd
+++ scene/menu/settings.gd
@@ -1,6 +1,20 @@
 extends Control
 
+@onready var sliders = {
+	0: $MarginContainer/VBoxContainer/VBoxContainer/MasterSlider,
+	1: $MarginContainer/VBoxContainer/VBoxContainer/MusicSlider,
+	2: $MarginContainer/VBoxContainer/VBoxContainer/SFXSlider,
+}
+
+func _ready() -> void:
+	load_slider_values()
+
 func _on_apply_pressed() -> void:
-	AudioServer.set_bus_volume_db(0, linear_to_db($MarginContainer/VBoxContainer/VBoxContainer/MasterSlider.value))
-	AudioServer.set_bus_volume_db(1, linear_to_db($MarginContainer/VBoxContainer/VBoxContainer/MusicSlider.value))
-	AudioServer.set_bus_volume_db(2, linear_to_db($MarginContainer/VBoxContainer/VBoxContainer/SFXSlider.value))
+	for bus_idx in sliders:
+		var slider_value = sliders[bus_idx].value
+		GlobalAudio.set_bus_volume_db(bus_idx, linear_to_db(slider_value))
+
+func load_slider_values() -> void:
+	for bus_idx in sliders:
+		var bus_value = GlobalAudio.get_bus_volume_db(bus_idx)
+		sliders[bus_idx].value = db_to_linear(bus_value)
