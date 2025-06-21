24ebf6d00f71fd7e0911985dfb7e788b76b99ec7
KacperPach <39103506+KacperPach@users.noreply.github.com>
2025-06-21 23:11:10 +0200

    Bugfix (#57)
    * winscreen
    
    * prezentacja

--- objects/ui/prep_phase_ui/prep_phase_ui.gd
+++ objects/ui/prep_phase_ui/prep_phase_ui.gd
@@ -2,13 +2,19 @@ extends Control
 
 @onready var start_wave_button = $StartWave
 @onready var money_label = $MoneyLabel
+@onready var win_labels: Control = $"win labels"
 
 func _ready():
 	start_wave_button.pressed.connect(_on_start_wave_pressed)
 	GlobalVars.money_changed.connect(update_money)
 	update_money(GlobalVars.money)
 
+func check_win_screen():
+	if (GlobalVars.wave == 4): 
+		win_labels.visible = true
+
 func _on_start_wave_pressed():
+	win_labels.visible = false
 	var new_song = preload("res://assets/audio/music/the-wandering-samurai-344699.mp3")
 	GlobalAudio.play_music(new_song)
 	get_parent().get_parent().start_wave()
